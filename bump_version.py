54550ff5ce1ab32a9262e2bbd7f0ee45614571a5
Alex Badics <alex.badics@balabit.com>
2017-12-06 15:59:14 +0100

    project: add LGPL v2.1 licence and licence checking
    Change-Id: Iddc2f9b6dce48f4388b79d7c16343b2c23afe223

--- bump_version.py
+++ bump_version.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+# Copyright (c) 2016-2017 Balabit
 #
-# Copyright (c) 2006-2017 Balabit
-# All Rights Reserved.
+# This file is part of Furnace.
+#
+# Furnace is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Furnace is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Furnace.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 from pathlib import Path
--- furnace/__init__.py
+++ furnace/__init__.py
@@ -1,6 +1,20 @@
 #
-# Copyright (c) 2006-2017 Balabit
-# All Rights Reserved.
+# Copyright (c) 2016-2017 Balabit
+#
+# This file is part of Furnace.
+#
+# Furnace is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Furnace is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Furnace.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 from .context import ContainerContext  # NOQA: F401 '.context.ContainerContext' imported but unused
