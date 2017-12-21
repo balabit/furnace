cfdc03960974c35ed78b3fcb8e9361be44ef392c
Alex Badics <alex.badics@balabit.com>
2017-12-21 16:53:31 +0100

    setup.py: add PyPI-specific options
    Change-Id: I9a6d19579cf771c251fdde2382f985f1957fa79c

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Furnace.  If not, see <http://www.gnu.org/licenses/>.
 #
+import os
 import sys
 from setuptools import setup, find_packages
 from furnace.version import get_version
@@ -31,9 +32,24 @@ dev_requirements = [
     'pytest==3.2.3',
 ]
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='furnace',
     version=get_version(),
+    description='A lightweight pure-python container implementation',
+    long_description=long_description,
+    url='https://github.com/balabit/furnace',
+    keywords='containers containerization',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: System',
+    ],
     packages=find_packages(include=["furnace*"]),
     extras_require={
         'dev': dev_requirements
